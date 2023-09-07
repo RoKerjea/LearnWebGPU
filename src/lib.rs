5b9859871bf2bd978b7a1397666cbecb55b8ea9a
RoKerjea <rokerjea@student.42.fr>
2023-09-07 20:41:55 +0200

    Surface step done, window print grey

--- src/lib.rs
+++ src/lib.rs
@@ -113,8 +113,28 @@ impl State {
 			label: Some("Render Encoder"),
 		});
 		{
-			let _render_pass = encoder
+			let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+				label: Some("Render Pass"),
+				color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+					view: &view,
+					resolve_target: None,
+					ops: wgpu::Operations {
+						load: wgpu::LoadOp::Clear(wgpu::Color {
+							r: 0.1,
+							g: 0.2,
+							b: 0.3,
+							a: 1.0,
+						}),
+						store: true,
+					},
+				})],
+				depth_stencil_attachment: None,
+			});
 		}
+
+		self.queue.submit(std::iter::once(encoder.finish()));
+		output.present();
+		Ok(())
 	}
 }
 
@@ -177,6 +197,18 @@ pub async fn run() {
 					}
 					_ => {}
 				}
+			}
+			Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+				state.update();
+				match state.render() {
+					Ok(_) => {}
+					Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+					Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+					Err(e) =>eprintln!("{:?}", e),
+				}
+			}
+			Event::MainEventsCleared => {
+				state.window().request_redraw();
 			}
 			_ => {}
 		}
