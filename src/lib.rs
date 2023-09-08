910708899b377c83139683f3f53b29c44a46617f
RoKerjea <rokerjea@student.42.fr>
2023-09-08 20:29:53 +0200

    start of texture step. done until getdatattotextur

--- src/lib.rs
+++ src/lib.rs
@@ -81,7 +81,7 @@ impl State {
         // The surface needs to live as long as the window that created it.
         // State owns the window so this should be safe.
 		let surface = unsafe { instance.create_surface(&window) }.unwrap();
-
+		
 		let adapter = instance.request_adapter(
 			&wgpu::RequestAdapterOptions {
 				power_preference: wgpu::PowerPreference::default(),
@@ -122,6 +122,30 @@ impl State {
 			view_formats: vec![],
 		};
 		surface.configure(&device, &config);
+
+		let diffuse_bytes = include_bytes!("happy-tree.png");
+		let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+		let diffuse_rgba = diffuse_image.to_rgba8();
+
+		use image::GenericImageView;
+		let dimensions = diffuse_image.dimensions();
+		let texture_size = wgpu::Extent3d {
+			width: dimensions.0,
+			height: dimensions.1,
+			depth_or_array_layers: 1,
+		};
+		let diffuse_texture = device.create_texture(
+			&wgpu::TextureDescriptor {
+				size: texture_size,
+				mip_level_count: 1,
+				sample_count: 1,
+				dimension: wgpu::TextureDimension::D2,
+				format: wgpu::TextureFormat::Rgba8UnormSrgb,
+				usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+				label: Some("diffuse_texture"),
+				view_formats: &[],
+			}
+		);
 		let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
 			label: Some("Shader"),
 			source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
