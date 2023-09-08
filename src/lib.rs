58bceb74b286871fe7cdec3b5ce876e8ad946f5b
RoKerjea <rokerjea@student.42.fr>
2023-09-08 20:00:47 +0200

    End of Buffer step, pink pentagon print to window

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #[cfg(target_arch="wasm32")]
 use wasm_bindgen::prelude::*;
 
+use wgpu::util::DeviceExt;
 use winit::window::Window;
 
 use winit::{
@@ -10,6 +11,48 @@ use winit::{
     window::WindowBuilder,
 };
 
+#[repr(C)]
+#[derive(Clone, Copy, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct  Vertex {
+	position: [f32; 3],
+	color: [f32; 3],
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+
+const INDICES: &[u16] = &[
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+];
+
+impl Vertex {
+	fn desc() -> wgpu::VertexBufferLayout<'static> {
+		wgpu::VertexBufferLayout {
+			array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+			step_mode: wgpu::VertexStepMode::Vertex,
+			attributes: &[
+				wgpu::VertexAttribute {
+					offset: 0,
+					shader_location: 0,
+					format: wgpu::VertexFormat::Float32x3,
+				},
+				wgpu::VertexAttribute {
+					offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+					shader_location: 1,
+					format: wgpu::VertexFormat::Float32x3,
+				}
+			]
+		}
+	}
+}
+
 struct State {
 	surface: wgpu::Surface,
 	device: wgpu::Device,
@@ -17,7 +60,10 @@ struct State {
 	config: wgpu::SurfaceConfiguration,
 	size: winit::dpi::PhysicalSize<u32>,
 	render_pipeline: wgpu::RenderPipeline,
+	vertex_buffer: wgpu::Buffer,
 	window: Window,
+	index_buffer: wgpu::Buffer,
+	num_indices: u32,
 }
 
 impl State {
@@ -92,7 +138,7 @@ impl State {
 			vertex: wgpu::VertexState {
 				module: &shader,
 				entry_point: "vs_main",
-				buffers: &[],
+				buffers: &[Vertex::desc()],
 			},
 			fragment: Some(wgpu::FragmentState {
 				module: &shader,
@@ -120,6 +166,21 @@ impl State {
 			},
 			multiview: None,
 		});
+		let vertex_buffer = device.create_buffer_init(
+			&wgpu::util::BufferInitDescriptor {
+				label: Some("Vertex Buffer"),
+				contents: bytemuck::cast_slice(VERTICES),
+				usage: wgpu::BufferUsages::VERTEX,
+			}
+		);
+		let index_buffer = device.create_buffer_init(
+			&wgpu::util::BufferInitDescriptor {
+				label: Some("Index Buffer"),
+				contents: bytemuck::cast_slice(INDICES),
+				usage: wgpu::BufferUsages::INDEX,
+			}
+		);
+		let num_indices = INDICES.len() as u32;
 		Self {
 			window,
 			surface,
@@ -127,7 +188,10 @@ impl State {
 			queue,
 			config,
 			size,
+			vertex_buffer,
 			render_pipeline,
+			index_buffer,
+			num_indices,
 		}
 	}
 
@@ -177,7 +241,9 @@ impl State {
 				depth_stencil_attachment: None,
 			});
 			render_pass.set_pipeline(&self.render_pipeline);
-			render_pass.draw(0..3, 0..1);
+			render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+			render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+			render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
 		}
 
 		self.queue.submit(std::iter::once(encoder.finish()));
